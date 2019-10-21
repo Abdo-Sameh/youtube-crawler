850d80402a4251b223eb45e954647c03b8cb12e7
Abdo-Sameh <abdo.sameh2000@gmail.com>
2019-10-21 16:55:40 +0200

    download thumbnails and full size images, save local path

--- controllers/videos_controller.py
+++ controllers/videos_controller.py
@@ -1,3 +1,6 @@
+import os
+import requests
+
 from crawler.video_crawler import VideoCrawler
 from models.video import *
 
@@ -11,18 +14,30 @@ class VideosController:
         return {'duration': data['items'][0]['contentDetails']['duration'],
                 'view_count': data['items'][0]['statistics']['viewCount']}
 
+    def download_image(self, url, video_id):
+        download_path = 'images/' + video_id
+        if not os.path.exists(download_path):
+            os.makedirs(download_path)
+        response = requests.get(url)
+        with open(download_path + '/' + url.split('/')[-1], 'wb') as f:
+            f.write(response.content)
+        return os.getcwd() + '/' + download_path
+
     def save_data(self):
         for i in range(len(self.items)):
             more_info = self.get_details(self.items[i]['snippet']['resourceId']['videoId'])
+            video_id = self.items[i]['snippet']['resourceId']['videoId']
             new_video = Video(
-                video_url='https://www.youtube.com/watch?v=' + self.items[i]['snippet']['resourceId']['videoId'],
+                video_url='https://www.youtube.com/watch?v=' + video_id,
                 title=self.items[i]['snippet']['title'],
                 thumbnail_url=self.items[i]['snippet']['thumbnails']['default']['url'],
                 full_image_url=self.items[i]['snippet']['thumbnails']['standard']['url']
             )
+            thumbnail_path = self.download_image(self.items[i]['snippet']['thumbnails']['default']['url'], video_id)
+            full_image_path = self.download_image(self.items[i]['snippet']['thumbnails']['standard']['url'], video_id)
             new_video.duration = more_info['duration']
             new_video.views = more_info['view_count']
-            new_video.downloaded_thumbnail_path = ''
-            new_video.downloaded_image_path = ''
+            new_video.downloaded_thumbnail_path = thumbnail_path
+            new_video.downloaded_image_path = full_image_path
             db.session.add(new_video)
         db.session.commit()
